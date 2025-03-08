b35f174afc4d8ca809c2ea8fd868c07cfbe4cfa5
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2025-03-08 22:03:03 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,39 @@
+from colorama import Fore , Style , init
+import time
+from sysadminx import VideoToAudioConverter
+
+init(autoreset=True)
+
+text = """
+
+ │█████╗██╗   ██╗███████╗ █████╗ ██████╗ ███╗   ███╗██╗███╗   ██╗██╗  ██╗
+ │╔════╝╚██╗ ██╔╝██╔════╝██╔══██╗██╔══██╗████╗ ████║██║████╗  ██║╚██╗██╔╝
+ │█████╗ ╚████╔╝ ███████╗███████║██║  ██║██╔████╔██║██║██╔██╗ ██║ ╚███╔╝
+ │═══██║  ╚██╔╝  ╚════██║██╔══██║██║  ██║██║╚██╔╝██║██║██║╚██╗██║ ██╔██╗
+ │█████║   ██║   ███████║██║  ██║██████╔╝██║ ╚═╝ ██║██║██║ ╚████║██╔╝ ██╗
+ │═════╝   ╚═╝   ╚══════╝╚═╝  ╚═╝╚═════╝ ╚═╝     ╚═╝╚═╝╚═╝  ╚═══╝╚═╝  ╚═╝
+ code by: https://github.com/MrTG-CodeBot
+ 1 Video To Audio
+"""
+for line in text.splitlines():
+    print(Fore.RED + line , end='\r' , flush=True)
+    time.sleep(0.1)
+    print(Fore.RED + line , flush=True)
+
+choice = input("Enter your choice: ")
+if choice == "1":
+    converter = VideoToAudioConverter()
+
+    video_path = input("Enter the path to the video file: ")
+    audio_path = input("Enter the path to save the audio file: ")
+
+    if '"' in video_path or audio_path:
+        video_path = video_path.replace('"' , '')
+        audio_path = audio_path.replace('"' , '')
+
+    audio_file , success , error_message = converter.convert_to_audio(video_path , audio_path)
+
+    if success:
+        print(Fore.RED + f"Audio file saved to: {audio_file}")
+    else:
+        print(Fore.RED + f"Conversion failed: {error_message}")

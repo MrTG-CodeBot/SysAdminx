844abb3dea97444e7e290469f7b5c89c914aa63d
MrTG-CodeBot <148360334+MrTG-CodeBot@users.noreply.github.com>
2025-03-11 03:54:43 +0530

    Update Docs2pdf.py

--- sysadminx/Docs2pdf.py
+++ sysadminx/Docs2pdf.py
@@ -1,14 +1,14 @@
 import docx2pdf
 from docx2pdf import convert
 
-class Docs2pdf:
-  def __init__(self):
-    pass
-    
-  def convert2pdf(self,inputpath:str,outputpath:str) -> str or None:
-    try:
-      return convert(inputpath, outputpath)
-    except Exception as e:
-      print(f"An error occured while converting to pdf: {e}")
-      return None
 
+class Docs2pdf:
+    def __init__(self):
+        pass
+
+    def convert2pdf(self , inputpath: str , outputpath: str) -> tuple[str or bool or None]:
+        try:
+            return True , convert(inputpath , outputpath)
+        except Exception as e:
+            print(f"An error occured while converting to pdf: {e}")
+            return False , None
